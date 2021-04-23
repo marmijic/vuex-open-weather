c2ddee4b66a2e433d1c451c7b0891e21a48b038c
Mario M <marmijic@gmail.com>
2021-04-24 01:44:13 +0200

    Adapt app style

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,21 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ffffff;
+  height: 100%;
+}
+.router-wrapper {
+  background-image: url("./assets/clouds.jpg");
+  height: 100%;
+  min-height: 100vh;
+  padding: 50px 0;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,17 +6,20 @@
       v-bind:key="index"
       v-on:click="navigateTo(cityWeather.id)"
     >
-      <p class="city-name">{{ cityWeather.name }}</p>
-      <img
-        :src="
-          'http://openweathermap.org/img/w/' +
-            cityWeather?.weather[0].icon +
-            '.png'
-        "
-        :alt="cityWeather.name"
-      />
-      <p class="temperature">{{ cityWeather?.main?.temp }} &#8451;</p>
-      <div class="temperatue"></div>
+      <p class="city-name">
+        <b>{{ cityWeather.name }}</b>
+      </p>
+      <div class="weather">
+        <img
+          :src="
+            'http://openweathermap.org/img/w/' +
+              cityWeather?.weather[0].icon +
+              '.png'
+          "
+          :alt="cityWeather.name"
+        />
+        <p class="temperature">{{ cityWeather?.main?.temp }} &#8451;</p>
+      </div>
     </div>
   </div>
 </template>
@@ -54,16 +57,41 @@ export default {
 }
 
 .city-weather {
-  background: #ffffff;
-  border-radius: 3px;
+  background-color: rgba(255, 255, 255, 0.6);
+  border-radius: 6px;
   border: 1px solid #59656f;
   box-sizing: border-box;
-  width: 150px;
+  width: 200px;
+  height: 200px;
   margin-right: 15px;
+  cursor: pointer;
 }
 
-.city-weather > p {
+.city-weather p {
   color: #2c4251;
+  font-size: 25px;
+}
+
+.city-weather > .weather {
+  display: flex;
+  align-items: center;
+}
+
+.city-weather > .weather > img {
+  height: 80px;
+}
+
+.city-weather > .weather > p {
+  font-weight: bold;
+}
+
+.city-weather:last-child {
+  margin-right: 0;
+}
+
+.city-weather:hover {
+  border-color: #e6e6ea;
+  background-color: #f4f4f8;
 }
 
 .city-weather:last-child {
